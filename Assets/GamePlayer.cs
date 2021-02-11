31bcf2e09366da5755340da09d90eca69f4547b6
YuyaSuzuki <yuya.suzuki0829@gmail.com>
2021-02-11 23:30:52 +0900

    キューブの操作と同期を確認

---
+++ Assets/GamePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GamePlayer : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    float velocity = 5f;
+    // Start is called before the first frame update
+    void Update()
+    {
+        // 自身が生成したオブジェクトのみに移動処理
+        if (photonView.IsMine)
+        {
+            float inputX = Input.GetAxis("Horizontal");
+            float inputY = Input.GetAxis("Vertical");
+            Vector3 dir = Vector3.Scale(new Vector3(inputX, inputY, 0).normalized, new Vector3(Mathf.Abs(inputX), Mathf.Abs(inputY), 1));
+            Vector3 dv = velocity * dir * Time.deltaTime;
+
+            transform.Translate(dv.x, dv.y, 0f);
+        }
+    }
+}

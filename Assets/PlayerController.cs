3598470e28e50d3d3e53b11fbccb1c2a903af234
YuyaSuzuki <yuya.suzuki0829@gmail.com>
2021-02-12 09:19:37 +0900

    スタミナの実装と表示の同期を追加

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
+{
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    [SerializeField]
+    private float _velocity = 5f;
+
+    /// <summary>
+    /// 最大スタミナ
+    /// </summary>
+    [SerializeField]
+    private float _maxStamina = 6f;
+
+    /// <summary>
+    /// スタミナ減少率
+    /// </summary>
+    [SerializeField]
+    private float _decreaseRate = 1f;
+
+    /// <summary>
+    /// スタミナ回復率
+    /// </summary>
+    [SerializeField]
+    private float _recoveryRate = 2f;
+
+    /// <summary>
+    /// スタミナ表示バーオブジェクト
+    /// </summary>
+    [SerializeField]
+    private Slider _staminaBar = default;
+
+    /// <summary>
+    /// 現在のスタミナ
+    /// </summary>
+    private float _currentStamina = 0f;
+
+    void Start()
+    {
+        // スタミナの初期化
+        _currentStamina = _maxStamina;
+        // スタミナ表示オブジェクトの取得
+        if (_staminaBar == null)
+        {
+            GameObject statusUi = transform.Find("StatusUi").gameObject;
+            _staminaBar = statusUi.transform.Find("StaminaBar").GetComponent<Slider>();
+        }
+    }
+
+    void Update()
+    {
+        // 自身が生成したオブジェクト
+        if (photonView.IsMine)
+        {
+            // 移動方向
+            float inputX = Input.GetAxis("Horizontal");
+            float inputY = Input.GetAxis("Vertical");
+            Vector3 dir = Vector3.Scale(new Vector3(inputX, inputY, 0).normalized,
+                                        new Vector3(Mathf.Abs(inputX), Mathf.Abs(inputY), 1));
+            Vector3 dv = dir * _velocity * Time.deltaTime;
+
+            // 移動あり
+            if (dv.sqrMagnitude > 0f)
+            {
+                // スタミナを減少
+                _currentStamina = Mathf.Max(0f, _currentStamina - Time.deltaTime*_decreaseRate);
+                // 座標の移動
+                transform.Translate(dv.x, dv.y, 0f);
+            }
+            // 移動なし
+            else
+            {
+                // スタミナを回復
+                _currentStamina = Mathf.Min(_currentStamina + Time.deltaTime*_recoveryRate, _maxStamina);
+            }
+        }
+
+        // スタミナをゲージに表示
+        _staminaBar.value = _currentStamina / _maxStamina;
+    }
+
+    /// <summary>
+    /// パラメータのリアルタイム同期
+    /// </summary>
+    /// <param name="stream"></param>
+    /// <param name="info"></param>
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        // 書き込み
+        if (stream.IsWriting)
+        {
+            // 自身のアバターのスタミナを送信
+            stream.SendNext(_currentStamina);
+        }
+        // 読み込み
+        else
+        {
+            // 他プレイヤーのアバターのスタミナを受信
+            _currentStamina = (float)stream.ReceiveNext();
+        }
+    }
+}
--- Assets/SampleScene.cs
+++ Assets/SampleScene.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 // MonoBehaviourPunCallbacksを継承して、PUNのコールバックを受け取れるようにする
 public class SampleScene : MonoBehaviourPunCallbacks
 {
-    private void Start() {
+    private void Start()
+    {
         // プレイヤー自身の名前を"Player"に設定する
         PhotonNetwork.NickName = "Player";
         // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
@@ -13,13 +14,15 @@ public class SampleScene : MonoBehaviourPunCallbacks
     }
 
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
-    public override void OnConnectedToMaster() {
+    public override void OnConnectedToMaster()
+    {
         // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
         PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
     }
 
     // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
-    public override void OnJoinedRoom() {
+    public override void OnJoinedRoom()
+    {
         // ランダムな座標に自身のネットワークオブジェクトを生成する
         var position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
         PhotonNetwork.Instantiate("GamePlayer", position, Quaternion.identity);

0a69e34909f2b7cb03735aa428dfc695e1514b5f
YuyaSuzuki <yuya.suzuki0829@gmail.com>
2021-02-14 06:56:25 +0900

    クリックした方向に弾を発射する昨日を追加

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector3 velocity;
+
+    public void Init(Vector3 origin, Vector3 direction)
+    {
+        transform.position = origin;
+        velocity = 9f * direction;
+    }
+
+    private void Update()
+    {
+        transform.Translate(velocity * Time.deltaTime);
+    }
+
+    // 画面外に移動したら削除する
+    // （Unityのエディター上では、シーンピューの画面も影響するので注意）
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerFireBullet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class PlayerFireBullet : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Bullet bulletPrefab = default;
+
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            // 左クリックでカーソル方向に弾を発射する
+            if (Input.GetMouseButtonDown(0))
+            {
+                Camera mainCamera = Camera.main;
+                var distance = Vector3.Distance(transform.position, mainCamera.gameObject.transform.position);
+                var mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+                var currentPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+                var direction = currentPosition - transform.position;
+
+                FireBullet(direction);
+            }
+        }
+    }
+
+    // 弾を発射するメソッド
+    private void FireBullet(Vector3 direction)
+    {
+        var bullet = Instantiate(bulletPrefab);
+        bullet.Init(transform.position, direction);
+    }
+}
